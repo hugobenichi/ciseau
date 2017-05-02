74aa83625824282c432affe0bf02672e3722904d
Hugo Benichi <hugobenichi@gmail.com>
2017-05-02 23:11:01 +0900

    fix stupid typo in project.clj

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
 (defproject ciseau "0.1.0-SNAPSHOT"
   :description "An experiment making a text editor in clojure"
-  :url "http://github.com/hugobenichi/ciseau
+  :url "http://github.com/hugobenichi/ciseau"
   :dependencies [[org.clojure/clojure "1.8.0"]])
