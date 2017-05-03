90818521aba225c33e87f0106cdd97a6ce1fca0c
Hugo Benichi <hugobenichi@gmail.com>
2017-05-03 10:16:41 +0900

    query tty dimensions [wip]

--- src/ciseau/core.clj
+++ src/ciseau/core.clj
@@ -1,7 +1,18 @@
 (ns ciseau.core)
 
+(defn sh [command]
+  ; FIXME
+  (with-out-str (util/cmd "/bin/sh" "-c" aommdn)))
+
+(defn dimensions
+  []
+  (let [dims (sh "/bin/stty size </dev/tty")
+        [h w] (map parse (str/split (clojure.string/trim-newline dims) #" "))]
+    {:rows h :columns w}))
+
 (defn -main
   [& args]
   (def files (map slurp args))
+  (println (dimensions))
   (doseq [f files]
     (println f)))
