ea00fa864e4cf04cf8f76b3f2811ba357843fa10
Hugo Benichi <hugobenichi@gmail.com>
2017-07-28 22:47:55 +0900

    Hello world ocaml
    Beginning of experimenting with ocaml for implementing ciseau

---
+++ ocaml/ciseau.ml
@@ -0,0 +1,11 @@
+(* TODOs:
+ *  - print the 6x6x6 color gradients
+ *  - put terminal in raw mode, do stuff, exist
+ *  - in raw mode, capture and print input
+ *)
+
+let main () =
+  print_string "hello world\n"
+;;
+
+main ()
