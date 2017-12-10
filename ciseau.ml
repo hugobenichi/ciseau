6b3fa14fd79d8f848e0b8fa9da7312935a808d67
Hugo Benichi <hugobenichi@gmail.com>
2017-12-11 00:05:26 +0900

    Fix the basic tokenizer
    Next steps would be to
      incorporate it into the Filebuffer
      use atom list instead of string for displaying
      add a mode to cycle background colors of atoms based on kind
    
    That way it quickly allows to see if this is a relevant way to look
    at files
    
    After that, the block layering cincepts needs some serious thoughts and
    design. Especially how the layers interacts with blocks modifications.

--- ciseau.ml
+++ ciseau.ml
@@ -82,7 +82,29 @@ let slurp f =
 
 module Atom = struct
 
-  type atom_kind = Text | Digit | Spacing | Operator | Structure | Line | Control | Other
+  type atom_kind = Text | Digit | Spacing | Operator | Structure | Line | Control | Other | Ending
+
+  let atom_kind_to_string = function
+    | Text      -> "Text"
+    | Digit     -> "Digit"
+    | Spacing   -> "Spacing"
+    | Operator  -> "Operator"
+    | Structure -> "Structure"
+    | Line      -> "Line"
+    | Control   -> "Control"
+    | Other     -> "Other"
+    | Ending    -> "Ending"
+
+  let atom_kind_to_string_padded = function
+    | Text      -> "Text      "
+    | Digit     -> "Digit     "
+    | Spacing   -> "Spacing   "
+    | Operator  -> "Operator  "
+    | Structure -> "Structure "
+    | Line      -> "Line      "
+    | Control   -> "Control   "
+    | Other     -> "Other     "
+    | Ending    -> "Ending    "
 
   let atom_kind_table = Array.make 128 Other ;;
   (* control codes *)
@@ -91,130 +113,128 @@ module Atom = struct
   atom_kind_table.(010 (* line feed *) ) = Line ;;
   atom_kind_table.(013 (* carriage return *) ) = Line ;;
   (* printable codes *)
-  atom_kind_table.(032 (* ' ' *) ) = Spacing ;;
-  atom_kind_table.(033 (* '!' *) ) = Operator ;;
-  atom_kind_table.(034 (* '"' *) ) = Structure ;;
-  atom_kind_table.(035 (* '#' *) ) = Operator ;;
-  atom_kind_table.(036 (* '$' *) ) = Operator ;;
-  atom_kind_table.(037 (* '%' *) ) = Operator ;;
-  atom_kind_table.(038 (* '&' *) ) = Operator ;;
-  atom_kind_table.(039 (* ''' *) ) = Structure ;;
-  atom_kind_table.(040 (* '(' *) ) = Structure ;;
-  atom_kind_table.(041 (* ')' *) ) = Structure ;;
-  atom_kind_table.(042 (* '*' *) ) = Operator ;;
-  atom_kind_table.(043 (* '+' *) ) = Operator ;;
-  atom_kind_table.(044 (* ',' *) ) = Operator ;;
-  atom_kind_table.(045 (* '-' *) ) = Operator ;;
-  atom_kind_table.(046 (* '.' *) ) = Operator ;;
-  atom_kind_table.(047 (* '/' *) ) = Operator ;;
-  atom_kind_table.(048 (* '0' *) ) = Digit ;;
-  atom_kind_table.(049 (* '1' *) ) = Digit ;;
-  atom_kind_table.(050 (* '2' *) ) = Digit ;;
-  atom_kind_table.(051 (* '3' *) ) = Digit ;;
-  atom_kind_table.(052 (* '4' *) ) = Digit ;;
-  atom_kind_table.(053 (* '5' *) ) = Digit ;;
-  atom_kind_table.(054 (* '6' *) ) = Digit ;;
-  atom_kind_table.(055 (* '7' *) ) = Digit ;;
-  atom_kind_table.(056 (* '8' *) ) = Digit ;;
-  atom_kind_table.(057 (* '9' *) ) = Digit ;;
-  atom_kind_table.(058 (* ':' *) ) = Operator ;;
-  atom_kind_table.(059 (* ';' *) ) = Operator ;;
-  atom_kind_table.(060 (* '<' *) ) = Operator ;;
-  atom_kind_table.(061 (* '=' *) ) = Operator ;;
-  atom_kind_table.(062 (* '>' *) ) = Operator ;;
-  atom_kind_table.(063 (* '?' *) ) = Operator ;;
-  atom_kind_table.(064 (* '@' *) ) = Operator ;;
-  atom_kind_table.(065 (* 'A' *) ) = Text ;;
-  atom_kind_table.(066 (* 'B' *) ) = Text ;;
-  atom_kind_table.(067 (* 'C' *) ) = Text ;;
-  atom_kind_table.(068 (* 'D' *) ) = Text ;;
-  atom_kind_table.(069 (* 'E' *) ) = Text ;;
-  atom_kind_table.(070 (* 'F' *) ) = Text ;;
-  atom_kind_table.(071 (* 'G' *) ) = Text ;;
-  atom_kind_table.(072 (* 'H' *) ) = Text ;;
-  atom_kind_table.(073 (* 'I' *) ) = Text ;;
-  atom_kind_table.(074 (* 'J' *) ) = Text ;;
-  atom_kind_table.(075 (* 'K' *) ) = Text ;;
-  atom_kind_table.(076 (* 'L' *) ) = Text ;;
-  atom_kind_table.(077 (* 'M' *) ) = Text ;;
-  atom_kind_table.(078 (* 'N' *) ) = Text ;;
-  atom_kind_table.(079 (* 'O' *) ) = Text ;;
-  atom_kind_table.(080 (* 'P' *) ) = Text ;;
-  atom_kind_table.(081 (* 'Q' *) ) = Text ;;
-  atom_kind_table.(082 (* 'R' *) ) = Text ;;
-  atom_kind_table.(083 (* 'S' *) ) = Text ;;
-  atom_kind_table.(084 (* 'T' *) ) = Text ;;
-  atom_kind_table.(085 (* 'U' *) ) = Text ;;
-  atom_kind_table.(086 (* 'V' *) ) = Text ;;
-  atom_kind_table.(087 (* 'W' *) ) = Text ;;
-  atom_kind_table.(088 (* 'X' *) ) = Text ;;
-  atom_kind_table.(089 (* 'Y' *) ) = Text ;;
-  atom_kind_table.(090 (* 'Z' *) ) = Text ;;
-  atom_kind_table.(091 (* '[' *) ) = Structure ;;
-  atom_kind_table.(092 (* '\' *) ) = Operator ;;
-  atom_kind_table.(093 (* ']' *) ) = Structure ;;
-  atom_kind_table.(094 (* '^' *) ) = Operator ;;
-  atom_kind_table.(095 (* '_' *) ) = Operator ;;
-  atom_kind_table.(096 (* '`' *) ) = Operator ;;
-  atom_kind_table.(097 (* 'a' *) ) = Text ;;
-  atom_kind_table.(098 (* 'b' *) ) = Text ;;
-  atom_kind_table.(099 (* 'c' *) ) = Text ;;
-  atom_kind_table.(100 (* 'd' *) ) = Text ;;
-  atom_kind_table.(101 (* 'e' *) ) = Text ;;
-  atom_kind_table.(102 (* 'f' *) ) = Text ;;
-  atom_kind_table.(103 (* 'g' *) ) = Text ;;
-  atom_kind_table.(104 (* 'h' *) ) = Text ;;
-  atom_kind_table.(105 (* 'i' *) ) = Text ;;
-  atom_kind_table.(106 (* 'j' *) ) = Text ;;
-  atom_kind_table.(107 (* 'k' *) ) = Text ;;
-  atom_kind_table.(108 (* 'l' *) ) = Text ;;
-  atom_kind_table.(109 (* 'm' *) ) = Text ;;
-  atom_kind_table.(110 (* 'n' *) ) = Text ;;
-  atom_kind_table.(111 (* 'o' *) ) = Text ;;
-  atom_kind_table.(112 (* 'p' *) ) = Text ;;
-  atom_kind_table.(113 (* 'q' *) ) = Text ;;
-  atom_kind_table.(114 (* 'r' *) ) = Text ;;
-  atom_kind_table.(115 (* 's' *) ) = Text ;;
-  atom_kind_table.(116 (* 't' *) ) = Text ;;
-  atom_kind_table.(117 (* 'u' *) ) = Text ;;
-  atom_kind_table.(118 (* 'v' *) ) = Text ;;
-  atom_kind_table.(119 (* 'w' *) ) = Text ;;
-  atom_kind_table.(120 (* 'x' *) ) = Text ;;
-  atom_kind_table.(121 (* 'y' *) ) = Text ;;
-  atom_kind_table.(122 (* 'z' *) ) = Text ;;
-  atom_kind_table.(123 (* '{' *) ) = Structure ;;
-  atom_kind_table.(124 (* '|' *) ) = Operator ;;
-  atom_kind_table.(125 (* '}' *) ) = Structure ;;
-  atom_kind_table.(126 (* '~' *) ) = Operator ;;
+  atom_kind_table.(032 (* ' ' *) ) <- Spacing ;;
+  atom_kind_table.(033 (* '!' *) ) <- Operator ;;
+  atom_kind_table.(034 (* '"' *) ) <- Structure ;;
+  atom_kind_table.(035 (* '#' *) ) <- Operator ;;
+  atom_kind_table.(036 (* '$' *) ) <- Operator ;;
+  atom_kind_table.(037 (* '%' *) ) <- Operator ;;
+  atom_kind_table.(038 (* '&' *) ) <- Operator ;;
+  atom_kind_table.(039 (* ''' *) ) <- Structure ;;
+  atom_kind_table.(040 (* '(' *) ) <- Structure ;;
+  atom_kind_table.(041 (* ')' *) ) <- Structure ;;
+  atom_kind_table.(042 (* '*' *) ) <- Operator ;;
+  atom_kind_table.(043 (* '+' *) ) <- Operator ;;
+  atom_kind_table.(044 (* ',' *) ) <- Operator ;;
+  atom_kind_table.(045 (* '-' *) ) <- Operator ;;
+  atom_kind_table.(046 (* '.' *) ) <- Operator ;;
+  atom_kind_table.(047 (* '/' *) ) <- Operator ;;
+  atom_kind_table.(048 (* '0' *) ) <- Digit ;;
+  atom_kind_table.(049 (* '1' *) ) <- Digit ;;
+  atom_kind_table.(050 (* '2' *) ) <- Digit ;;
+  atom_kind_table.(051 (* '3' *) ) <- Digit ;;
+  atom_kind_table.(052 (* '4' *) ) <- Digit ;;
+  atom_kind_table.(053 (* '5' *) ) <- Digit ;;
+  atom_kind_table.(054 (* '6' *) ) <- Digit ;;
+  atom_kind_table.(055 (* '7' *) ) <- Digit ;;
+  atom_kind_table.(056 (* '8' *) ) <- Digit ;;
+  atom_kind_table.(057 (* '9' *) ) <- Digit ;;
+  atom_kind_table.(058 (* ':' *) ) <- Operator ;;
+  atom_kind_table.(059 (* ';' *) ) <- Operator ;;
+  atom_kind_table.(060 (* '<' *) ) <- Operator ;;
+  atom_kind_table.(061 (* '=' *) ) <- Operator ;;
+  atom_kind_table.(062 (* '>' *) ) <- Operator ;;
+  atom_kind_table.(063 (* '?' *) ) <- Operator ;;
+  atom_kind_table.(064 (* '@' *) ) <- Operator ;;
+  atom_kind_table.(065 (* 'A' *) ) <- Text ;;
+  atom_kind_table.(066 (* 'B' *) ) <- Text ;;
+  atom_kind_table.(067 (* 'C' *) ) <- Text ;;
+  atom_kind_table.(068 (* 'D' *) ) <- Text ;;
+  atom_kind_table.(069 (* 'E' *) ) <- Text ;;
+  atom_kind_table.(070 (* 'F' *) ) <- Text ;;
+  atom_kind_table.(071 (* 'G' *) ) <- Text ;;
+  atom_kind_table.(072 (* 'H' *) ) <- Text ;;
+  atom_kind_table.(073 (* 'I' *) ) <- Text ;;
+  atom_kind_table.(074 (* 'J' *) ) <- Text ;;
+  atom_kind_table.(075 (* 'K' *) ) <- Text ;;
+  atom_kind_table.(076 (* 'L' *) ) <- Text ;;
+  atom_kind_table.(077 (* 'M' *) ) <- Text ;;
+  atom_kind_table.(078 (* 'N' *) ) <- Text ;;
+  atom_kind_table.(079 (* 'O' *) ) <- Text ;;
+  atom_kind_table.(080 (* 'P' *) ) <- Text ;;
+  atom_kind_table.(081 (* 'Q' *) ) <- Text ;;
+  atom_kind_table.(082 (* 'R' *) ) <- Text ;;
+  atom_kind_table.(083 (* 'S' *) ) <- Text ;;
+  atom_kind_table.(084 (* 'T' *) ) <- Text ;;
+  atom_kind_table.(085 (* 'U' *) ) <- Text ;;
+  atom_kind_table.(086 (* 'V' *) ) <- Text ;;
+  atom_kind_table.(087 (* 'W' *) ) <- Text ;;
+  atom_kind_table.(088 (* 'X' *) ) <- Text ;;
+  atom_kind_table.(089 (* 'Y' *) ) <- Text ;;
+  atom_kind_table.(090 (* 'Z' *) ) <- Text ;;
+  atom_kind_table.(091 (* '[' *) ) <- Structure ;;
+  atom_kind_table.(092 (* '\' *) ) <- Operator ;;
+  atom_kind_table.(093 (* ']' *) ) <- Structure ;;
+  atom_kind_table.(094 (* '^' *) ) <- Operator ;;
+  atom_kind_table.(095 (* '_' *) ) <- Operator ;;
+  atom_kind_table.(096 (* '`' *) ) <- Operator ;;
+  atom_kind_table.(097 (* 'a' *) ) <- Text ;;
+  atom_kind_table.(098 (* 'b' *) ) <- Text ;;
+  atom_kind_table.(099 (* 'c' *) ) <- Text ;;
+  atom_kind_table.(100 (* 'd' *) ) <- Text ;;
+  atom_kind_table.(101 (* 'e' *) ) <- Text ;;
+  atom_kind_table.(102 (* 'f' *) ) <- Text ;;
+  atom_kind_table.(103 (* 'g' *) ) <- Text ;;
+  atom_kind_table.(104 (* 'h' *) ) <- Text ;;
+  atom_kind_table.(105 (* 'i' *) ) <- Text ;;
+  atom_kind_table.(106 (* 'j' *) ) <- Text ;;
+  atom_kind_table.(107 (* 'k' *) ) <- Text ;;
+  atom_kind_table.(108 (* 'l' *) ) <- Text ;;
+  atom_kind_table.(109 (* 'm' *) ) <- Text ;;
+  atom_kind_table.(110 (* 'n' *) ) <- Text ;;
+  atom_kind_table.(111 (* 'o' *) ) <- Text ;;
+  atom_kind_table.(112 (* 'p' *) ) <- Text ;;
+  atom_kind_table.(113 (* 'q' *) ) <- Text ;;
+  atom_kind_table.(114 (* 'r' *) ) <- Text ;;
+  atom_kind_table.(115 (* 's' *) ) <- Text ;;
+  atom_kind_table.(116 (* 't' *) ) <- Text ;;
+  atom_kind_table.(117 (* 'u' *) ) <- Text ;;
+  atom_kind_table.(118 (* 'v' *) ) <- Text ;;
+  atom_kind_table.(119 (* 'w' *) ) <- Text ;;
+  atom_kind_table.(120 (* 'x' *) ) <- Text ;;
+  atom_kind_table.(121 (* 'y' *) ) <- Text ;;
+  atom_kind_table.(122 (* 'z' *) ) <- Text ;;
+  atom_kind_table.(123 (* '{' *) ) <- Structure ;;
+  atom_kind_table.(124 (* '|' *) ) <- Operator ;;
+  atom_kind_table.(125 (* '}' *) ) <- Structure ;;
+  atom_kind_table.(126 (* '~' *) ) <- Operator ;;
 
   let atom_kind_of = Char.code >> Array.get atom_kind_table
 
-  type atom_block = {
+  let atom_kind_at s i =
+    if i < slen s then (String.get s i) |> atom_kind_of else Ending
+
+  type atom = {
     kind  : atom_kind;
     line  : string;
     start : int;
     stop  : int;
   }
 
-  type atom = Atom of atom_block | Space | Tab | End_Of_Line
-
-  let atom_to_string = function
-    | Atom a      -> a.line
-    | Space       -> "'space'"
-    | Tab         -> "'tab'"
-    | End_Of_Line -> "'end_of_line'"
+  let atom_to_string a =
+    Printf.sprintf "%s:'%s'" (atom_kind_to_string_padded a.kind) (String.sub a.line a.start (a.stop - a.start))
 
   (* TODO: support utf8 as Text *)
   (* TODO: define grouping more properly. For instance Structure chars should never group *)
   let rec tokenize_atoms line all_atoms kind start index =
-    if index >= slen line
-    then End_Of_Line :: all_atoms
+    if kind = Ending
+    then List.rev all_atoms
     else
-      let next_kind = atom_kind_of (String.get line index) in
+      let next_kind = atom_kind_at line index in
       if next_kind = kind
       then tokenize_atoms line all_atoms kind start (index + 1)
       else
-        let a = Atom {
+        let a = {
           kind  = kind ;
           line  = line ;
           start = start ;
@@ -223,14 +243,15 @@ module Atom = struct
         tokenize_atoms line (a :: all_atoms) next_kind index (index + 1)
 
   let generic_atom_parser line =
-    let first_kind = if slen line = 0 then Other else atom_kind_of (String.get line 0)
-    in tokenize_atoms line [] first_kind 0 1
+    tokenize_atoms line [] (atom_kind_at line 0) 0 1
 
-  (* let example = "" *)
+  let println s = print_string s ; print_newline ()
 
-  (* let parsed_example = generic_atom_parser example *)
-
-  (* List.iter (atom_to_string >> print_string) parsed_example *)
+  let test () =
+    (* "let {x ; y } = cursor_offset <+> vec2 in" *)
+    "let cursor_hide           = start ^ \"?25l\" ;;"
+    |> generic_atom_parser
+    |> List.iter (atom_to_string >> println)
 
 end
 
@@ -1361,4 +1382,5 @@ module Ciseau = struct
 end
 
 let () =
-  Ciseau.main ()
+  Atom.test()
+  (* Ciseau.main () *)
