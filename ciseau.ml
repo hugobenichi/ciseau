c5044733927fc2ebc641209ed61af667139f4113
Hugo Benichi <hugobenichi@gmail.com>
2017-12-29 15:55:19 +0900

    More iterator: add array iterators and size_info
    This should be sufficient to implement a nice get_view.
    But the first next step is to get rid of Atom in LineInfo

--- ciseau.ml
+++ ciseau.ml
@@ -93,15 +93,24 @@ module Iter2 = struct
         cursor  : 'cursor ;
       }
 
-  (* TODO: use size info *)
-  type size_info = Empty | Unknown | Finite of int | Bounded of int | Infinite
+  type size_info =
+      Empty           (* an empty iterator *)
+    | Exact of int    (* an iterator with exactly n elements, like for instance an aray iterator *)
+    | Bounded of int  (* an iterator with at most n elements *)
+    | Finite          (* a finite iterator with unknown size, like for instance a list iterator *)
+    | Unknown         (* an iterator whose size is unknown. May be infinite *)
+
+  let size_info_finite x = Finite
+  let size_info_unknown x = Unknown
+  let size_info_empty x = Empty
+
   (* TODO: use next_info to implement efficient drop and range *)
   type next_info = Stop | PullOne | Pull of int | Drop of int | Limit of int
 
   type ('elem , 'cursor) iter_class = {
     first : ('elem , 'cursor) iter_seq ;
     next  : 'cursor -> ('elem , 'cursor) iter_seq ;
-    (* left  : 'cursor -> size_info *)
+    size  : 'cursor -> size_info ;
   }
 
   let mk_elem e c = Elem { elem = e ; cursor = c }
@@ -118,18 +127,30 @@ module Iter2 = struct
     | Elem { elem ; cursor }  -> loop (fn z elem) (next cursor)
     in loop zero first
 
-  let map fn { first ; next } =
+  let map fn { first ; next ; size } =
     let map_fn = function
     | End -> End
     | Elem { elem ; cursor } -> mk_elem (fn elem) cursor
-    in { first = map_fn first ; next = next >> map_fn }
+    in { first = map_fn first ; next = next >> map_fn ; size = size }
 
-  let filter fn { first ; next } =
+  let filter fn { first ; next ; size } =
+    let size' cursor =
+      match size cursor with
+      | Exact n -> Bounded n
+      | other -> other
+    in
     let rec loop = function
     | End -> End
     | Elem { elem ; cursor } as e ->
       if fn elem then e else cursor |> next |> loop
-    in { first = loop first ; next = next >> loop }
+    in { first = loop first ; next = next >> loop ; size = size' }
+
+  let drop n { first ; next ; size } =
+   let rec loop n = function
+     | End -> End
+     | e when n = 0 -> e
+     | Elem { cursor ; _ } -> loop (n - 1) (next cursor)
+   in { first = loop n first ; next = next ; size = size }
 
   let to_list it =
     it |> fold (flip List.cons) [] |> List.rev
@@ -138,7 +159,17 @@ module Iter2 = struct
     let next = function
       | [] -> End
       | h :: t -> mk_elem h t
-    in { first = next ls ; next = next }
+    in { first = next ls ; next = next ; size = size_info_finite }
+
+  let from_array_slice start stop a =
+    let next i =
+      if i < stop then mk_elem a.(i) (i + 1) else End
+    in
+    let size_info i = Exact (stop - i)
+    in { first = next start ; next = next ; size = size_info }
+
+  let from_array a =
+    from_array_slice 0 (alen a) a
 
   let test () =
     let print_iter x =
@@ -149,8 +180,18 @@ module Iter2 = struct
     in
     let l1 = [1 ; 2 ; 3 ; 5] in
     print_list l1 ;
+    l1 |> from_list |> drop 2 |> to_list |> print_list ;
     l1 |> from_list |> filter (fun x -> x < 3) |> to_list |> print_list ;
     l1 |> from_list |> map (fun x -> x * 2) |> to_list |> print_list ;
+
+    print_newline () ;
+    let a1 = [| 10 ; 11 ; 12 ; 13 ; 14 |] in
+    a1 |> from_array |> print_iter ;
+    a1 |> from_array |> drop 2 |> print_iter ;
+    a1 |> from_array |> filter (fun x -> x < 12 || x > 13) |> print_iter ;
+    a1 |> from_array |> map (fun x -> x / 2) |> print_iter ;
+    a1 |> from_array_slice 0 2 |> print_iter ;
+    a1 |> from_array_slice 2 5 |> print_iter ;
     ()
 
     let _ = test ()
