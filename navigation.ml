e7b0eb2503b79c7a6e6a7fa5e5a586fd70c08600
Hugo Benichi <hugobenichi@gmail.com>
2019-05-17 17:25:34 +0900

    navigation: flatten modules

--- navigation.ml
+++ navigation.ml
@@ -36,147 +36,133 @@ let zero_index_entry = { path = "" ; dtype = DT_UNKNOWN ; tokens = [] }
 
 let insert_time = ref 0.0
 
-module Navigator = struct
-  (* Note on different interface into range search:
-   *  - Takes 5 seconds to index Linux, 99% of it is file system exploration
-   *      total_entries=72169     total_entries_length=3279113
-   *      total_tokens=50022      total_tokens_length=637316
-   *  - is it worthwhile to try to do iterative search narrowing ?
-   *    - requires keeping some kind of cursor and update it, use code has to care
-   *    - try remove visibility into Suffixarray
-   *  - could base function just be find find_all_paths_by_token: string -> string iter / string array
-   *  - actually, at which point is an index necessary in the first place ?
-   *  - just having all paths in memory and brute force searching them might be just good enough
-   *)
+type filter_fn = string -> string -> bool
 
-  type filter_fn = string -> string -> bool
+type file_index = {
+  entries       : index_entry array ;
+}
 
-  type file_index = {
-    entries       : index_entry array ;
-  }
+type stats = {
+  total_entries         : int ;
+  total_entries_length  : int ;
+}
 
-  type stats = {
-    total_entries         : int ;
-    total_entries_length  : int ;
-  }
+type readdir_state = {
+  index_entry_buffer   : index_entry Arraybuffer.t ;
+  visit_queue   : string Queue.t ;
+  filter        : string -> string -> bool ;
+  buffer        : Buffer.t ;
+}
 
-  type readdir_state = {
-    index_entry_buffer   : index_entry Arraybuffer.t ;
-    visit_queue   : string Queue.t ;
-    filter        : string -> string -> bool ;
-    buffer        : Buffer.t ;
-  }
+let mk_readdir_rec_state path filter =
+  let index_entry_buffer = Arraybuffer.empty zero_index_entry in
+  let visit_queue = Queue.create () in
+  let buffer = Buffer.create 1024 in
+  Queue.push path visit_queue ;
+  { index_entry_buffer ; visit_queue ; filter ; buffer }
 
-  let mk_readdir_rec_state path filter =
-    let index_entry_buffer = Arraybuffer.empty zero_index_entry in
-    let visit_queue = Queue.create () in
-    let buffer = Buffer.create 1024 in
-    Queue.push path visit_queue ;
-    { index_entry_buffer ; visit_queue ; filter ; buffer }
+let nofilter anydir anyname = true
 
-  let nofilter anydir anyname = true
+let token_index_insert tbl entry token =
+  if slen token > 0 then (
+    insert_time -=. Sys.time () ;
+    Hashtbl.find_opt tbl token
+      |> Options.get_or []
+      |> List.cons entry
+      |> Hashtbl.replace tbl token ;
+    insert_time +=. Sys.time ()
+  )
 
-  let token_index_insert tbl entry token =
-    if slen token > 0 then (
-      insert_time -=. Sys.time () ;
-      Hashtbl.find_opt tbl token
-        |> Options.get_or []
-        |> List.cons entry
-        |> Hashtbl.replace tbl token ;
-      insert_time +=. Sys.time ()
-    )
+(* depth first directory walk. Caveats: can stack overflow or exhaust fds for very deep hierarchies. *)
+let rec readdir state tokens path =
+  match Unix.opendir path with
+    | dir ->
+        let buffer_n = Buffer.length state.buffer in
+        let go_on = ref true in
+        while !go_on do
+          match readdir_t dir with
+            | ("", _) -> go_on := false
+            | (".", _)
+            | ("..", _) -> ()
+            | (item, _) when not (state.filter path item) -> ()
+            | (item, d_type) when d_type = DT_DIR || d_type = DT_REG ->
+                begin
+                  Buffer.add_char state.buffer '/' ;
+                  Buffer.add_string state.buffer item ;
+                  let new_path = Buffer.contents state.buffer in
+                  let tokens' = item :: tokens in
+                  let entry = {
+                    dtype     = d_type ;
+                    path      = new_path ;
+                    tokens    = tokens' ;
+                  } in
+                  Arraybuffer.append state.index_entry_buffer entry ;
+                  if d_type == DT_DIR then
+                    readdir state tokens' new_path ;
+                  Buffer.truncate state.buffer buffer_n
+                end
+            | (_, _) -> ()
+      done ;
+      Unix.closedir dir
+  | exception Unix.Unix_error (Unix.EACCES, _, _)   -> ()
+  | exception Unix.Unix_error (Unix.ENOTDIR, _, _)  -> ()
+  | exception Unix.Unix_error (Unix.ENOENT, _, _)   -> ()
 
-  (* depth first directory walk. Caveats: can stack overflow or exhaust fds for very deep hierarchies. *)
-  let rec readdir state tokens path =
-    match Unix.opendir path with
-      | dir ->
-          let buffer_n = Buffer.length state.buffer in
-          let go_on = ref true in
-          while !go_on do
-            match readdir_t dir with
-              | ("", _) -> go_on := false
-              | (".", _)
-              | ("..", _) -> ()
-              | (item, _) when not (state.filter path item) -> ()
-              | (item, d_type) when d_type = DT_DIR || d_type = DT_REG ->
-                  begin
-                    Buffer.add_char state.buffer '/' ;
-                    Buffer.add_string state.buffer item ;
-                    let new_path = Buffer.contents state.buffer in
-                    let tokens' = item :: tokens in
-                    let entry = {
-                      dtype     = d_type ;
-                      path      = new_path ;
-                      tokens    = tokens' ;
-                    } in
-                    Arraybuffer.append state.index_entry_buffer entry ;
-                    if d_type == DT_DIR then
-                      readdir state tokens' new_path ;
-                    Buffer.truncate state.buffer buffer_n
-                  end
-              | (_, _) -> ()
-        done ;
-        Unix.closedir dir
-    | exception Unix.Unix_error (Unix.EACCES, _, _)   -> ()
-    | exception Unix.Unix_error (Unix.ENOTDIR, _, _)  -> ()
-    | exception Unix.Unix_error (Unix.ENOENT, _, _)   -> ()
+let mk_file_index ?filter:(filter=nofilter) path =
+  let state = mk_readdir_rec_state path filter in
+  Buffer.add_string state.buffer path ;
+  readdir state [] path ;
+  let index = { entries = Arraybuffer.to_array state.index_entry_buffer } in
+  Array.sort index_entry_compare index.entries ;
+  index
 
-  let mk_file_index ?filter:(filter=nofilter) path =
-    let state = mk_readdir_rec_state path filter in
-    Buffer.add_string state.buffer path ;
-    readdir state [] path ;
-    let index = { entries = Arraybuffer.to_array state.index_entry_buffer } in
-    Array.sort index_entry_compare index.entries ;
-    index
+let index_to_entries { entries } = Array.map index_entry_path entries
 
-  let index_to_entries { entries } = Array.map index_entry_path entries
+let file_index_stats { entries } = {
+  total_entries         = alen entries ;
+  total_entries_length  = Array.fold_left (fun bytecount { path } -> bytecount + (slen path)) 0 entries ;
+}
 
-  let file_index_stats { entries } = {
-    total_entries         = alen entries ;
-    total_entries_length  = Array.fold_left (fun bytecount { path } -> bytecount + (slen path)) 0 entries ;
-  }
+let rec make_matcher pattern =
+  match string_first pattern with
+    | '!' when pattern = "!"  ->  const true
+    | '!'   ->  neg (make_matcher (string_drop 1 pattern))
+    | '='   ->  (=) (string_drop 1 pattern) (* TODO: only match directory *)
+    | '/'   ->  string_starts_with (string_drop 1 pattern)
+    | '~'   ->
+        begin try
+          let regexp = Str.regexp (string_drop 1 pattern) in
+          fun token ->
+            begin match Str.search_forward regexp token 0 with
+              | _           -> true
+              | exception _ -> false
+            end
+        with
+          (* TODO: for incorrect regexps, show the pattern in red *)
+          e -> const true
+        end
+    | _     ->  string_is_substring pattern
 
-  let rec make_matcher pattern =
-    match string_first pattern with
-      | '!' when pattern = "!"  ->  const true
-      | '!'   ->  neg (make_matcher (string_drop 1 pattern))
-      | '='   ->  (=) (string_drop 1 pattern) (* TODO: only match directory *)
-      | '/'   ->  string_starts_with (string_drop 1 pattern)
-      | '~'   ->
-          begin try
-            let regexp = Str.regexp (string_drop 1 pattern) in
-            fun token ->
-              begin match Str.search_forward regexp token 0 with
-                | _           -> true
-                | exception _ -> false
-              end
-          with
-            (* TODO: for incorrect regexps, show the pattern in red *)
-            e -> const true
-          end
-      | _     ->  string_is_substring pattern
+(* patterns must not be empty *)
+let find_matches { entries } patterns =
+  let buffer = Arraybuffer.empty zero_index_entry in
+  let matchers = patterns |> List.filter ((<>) "") |> List.map make_matcher in
+  Array.iter (fun entry ->
+    if entry.dtype = DT_REG && List.for_all (fun matcher -> List.exists matcher entry.tokens) matchers
+      then Arraybuffer.append buffer entry
+  ) entries ;
+  Arraybuffer.to_array buffer
 
-  (* patterns must not be empty *)
-  let find_matches { entries } patterns =
-    let buffer = Arraybuffer.empty zero_index_entry in
-    let matchers = patterns |> List.filter ((<>) "") |> List.map make_matcher in
-    Array.iter (fun entry ->
-      if entry.dtype = DT_REG && List.for_all (fun matcher -> List.exists matcher entry.tokens) matchers
-        then Arraybuffer.append buffer entry
-    ) entries ;
-    Arraybuffer.to_array buffer
-
-  let start_matcher_thread file_index patterns_channel match_channel =
-    Thread.create (fun () ->
-      while true do
-        patterns_channel
-          |> Event.receive
-          |> Event.sync
-          |> find_matches file_index
-          |> Event.send match_channel
-          |> Event.sync
-      done) ()
-end
+let start_matcher_thread file_index patterns_channel match_channel =
+  Thread.create (fun () ->
+    while true do
+      patterns_channel
+        |> Event.receive
+        |> Event.sync
+        |> find_matches file_index
+        |> Event.send match_channel
+        |> Event.sync
+    done) ()
 
 let print_entries = Array.iter print_stringln
 
@@ -208,7 +194,7 @@ let navigation_test1 () =
   let filter anydir item = item <> ".git" in
   print_string base_path ; print_newline () ;
   let t1 = Sys.time () in
-  let file_index = Navigator.mk_file_index ~filter:filter base_path in
+  let file_index = mk_file_index ~filter:filter base_path in
   Printf.printf "exploration done: %f\n" ((Sys.time ()) -. t1) ;
   print_string "insert: " ; print_float !insert_time ; print_newline () ;
   let gc_stat2 = Gc.quick_stat () in
@@ -216,20 +202,20 @@ let navigation_test1 () =
     (gc_stat2.minor_collections - gc_stat.minor_collections)
     (gc_stat2.major_collections - gc_stat.major_collections) ;
   let {
-    Navigator.total_entries         ;
-    Navigator.total_entries_length  ;
-  } = Navigator.file_index_stats file_index in
+    total_entries         ;
+    total_entries_length  ;
+  } = file_index_stats file_index in
   Printf.printf "entries=%d entries_length=%d\n" total_entries total_entries_length ;
   let a1 = Sys.time () in
   let pattern = if alen Sys.argv > 2 then Sys.argv.(2) else "xfrm" in
-  Navigator.find_matches file_index [pattern] |> Array.map index_entry_path |> print_entries ;
+  find_matches file_index [pattern] |> Array.map index_entry_path |> print_entries ;
   Printf.printf "find_time: %f\n" ((Sys.time ()) -. a1)
 
 let navigation_test2 () =
   let open Term in
   let path = path_normalize (if alen Sys.argv > 1 then Sys.argv.(1) else "/etc") in
   let filter anydir item = item <> ".git" in
-  let file_index = Navigator.mk_file_index ~filter:filter path in
+  let file_index = mk_file_index ~filter:filter path in
   let next_key = Keys.make_next_key_fn () in
   let rec loop framebuffer path index pattern =
     next_key ()
@@ -247,7 +233,7 @@ let navigation_test2 () =
               loop framebuffer path index ""
         | Some new_pattern ->
               let t1 = Sys.time () in
-              let entries = Navigator.find_matches file_index (String.split_on_char ' '  new_pattern) in
+              let entries = find_matches file_index (String.split_on_char ' '  new_pattern) in
               let t2 = Sys.time () in
               let delta = t2 -. t1 in
               let footer = Printf.sprintf "%s (time %f, found %d, insert time %f)" new_pattern delta (alen entries) !insert_time in
@@ -273,11 +259,11 @@ let navigation_test3 () =
   let open Term in
   let path = path_normalize (if alen Sys.argv > 1 then Sys.argv.(1) else "/etc") in
   let filter anydir item = item <> ".git" in
-  let file_index = Navigator.mk_file_index ~filter:filter path in
+  let file_index = mk_file_index ~filter:filter path in
   let next_key = Keys.make_next_key_fn () in
   let patterns_channel = Event.new_channel () in
   let match_channel = Event.new_channel () in
-  let matcher_thread = Navigator.start_matcher_thread file_index patterns_channel match_channel in
+  let matcher_thread = start_matcher_thread file_index patterns_channel match_channel in
   let _ = matcher_thread in
   let patterns_event = ref (Event.always ()) in
   let rec loop framebuffer path index pattern last_match =
--- navigation.mli
+++ navigation.mli
@@ -1,21 +1,18 @@
-(* TODO: flatten into main namespace *)
-module Navigator : sig
-  (* Filter function for excluding directories or files from listing
-   * 1st arg: directory being listed
-   * 2nd arg: item found in the directory
-   * return value: true if the item should be listed *)
-  type filter_fn = string -> string -> bool
+(* Filter function for excluding directories or files from listing
+ * 1st arg: directory being listed
+ * 2nd arg: item found in the directory
+ * return value: true if the item should be listed *)
+type filter_fn = string -> string -> bool
 
-  type file_index
+type file_index
 
-  type stats = {
-    total_entries             : int ;
-    total_entries_length      : int ;
-  }
+type stats = {
+  total_entries             : int ;
+  total_entries_length      : int ;
+}
 
-  val mk_file_index           : ?filter:filter_fn -> string -> file_index
-  val index_to_entries        : file_index -> string array
-  val file_index_stats        : file_index -> stats
-end
+val mk_file_index           : ?filter:filter_fn -> string -> file_index
+val index_to_entries        : file_index -> string array
+val file_index_stats        : file_index -> stats
 
 val navigation_test           : unit -> unit
